0cec6a3339b7e6c559d261e0ef53760b4c6f0fe7
HarshiniReddy13 <harshinig.reddy@gmail.com>
2025-05-20 11:49:23 +0530

    change1

--- src/components/Payment.jsx
+++ src/components/Payment.jsx
@@ -76,7 +76,7 @@ const Payment = () => {
                     type="submit"
                     className="w-full bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700"
                 >
-                    Submit Payment
+                    Proceed to Pay
                 </button>
             </form>
         </div>
